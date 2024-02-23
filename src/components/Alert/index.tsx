635a00ed11633ce5901ceb37b32fe646d7672e6f
tabish615 <tabish615@gmail.com>
2024-02-24 01:34:00 +0500

    fixed spacing issue

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -15,7 +15,7 @@ const Alert: React.FC<AlertProps> = ({ type, message, onClose }) => {
 
     return (
         <div className={classes} role="alert">
-            <strong className="font-bold">{type === 'error' && 'Error'}</strong>
+            <strong className="font-bold">{type === 'error' && 'Error: '}</strong>
             <span className="block sm:inline">{message}</span>
             <span className="absolute top-0 bottom-0 right-0 px-4 py-3" onClick={onClose}>
                 <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
